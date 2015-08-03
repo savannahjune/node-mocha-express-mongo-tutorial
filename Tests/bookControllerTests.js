f73ec06534bb1f69cc3d2ee40a933f65da48b507
Savannah Henderson <savannahjune@gmail.com>
2015-08-03 12:57:32 -0700

    Finished tutorial from pluralsight.

---
+++ Tests/bookControllerTests.js
@@ -0,0 +1,28 @@
+var should = require('should'),
+    sinon = require('sinon');
+
+describe('Book controller tests:', function(){
+  describe('Post', function(){
+    it('should not allow an empty title on post', function(){
+      var Book = function(book){this.save = function(){};};
+
+      var req = {
+        body: {
+          author: 'Savannah'
+        }
+      };
+
+      var res = {
+        status: sinon.spy(),
+          send: sinon.spy()
+      };
+
+      var bookController = require('../controllers/bookController')(Book);
+
+      bookController.post(req, res);
+
+      res.status.calledWith(400).should.equal(true, 'Bad status ' + res.status.args);
+      res.send.calledWith('Title is required').should.equal(true);
+    });
+  });
+});
---
+++ app.js
@@ -0,0 +1,36 @@
+var express = require('express');
+    mongoose = require('mongoose');
+    bodyParser = require('body-parser');
+
+var db;
+if(process.env.ENV == 'Test'){
+  db = mongoose.connect('mongodb://localhost/bookAPI_test');
+}
+else{
+  db = mongoose.connect('mongodb://localhost/bookAPI');
+}
+
+var Book = require('./models/bookModel');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+bookRouter = require('./Routes/bookRoutes')(Book);
+
+app.use('/api/books', bookRouter);
+// app.use('/api/authors', authorRouter);
+
+app.get('/', function(req, res){
+  res.send('welcome to my API! Snitch');
+});
+
+app.listen(port, function(){
+  console.log('Gulp is running on PORT: ' + port);
+});
+
+module.exports = app;
